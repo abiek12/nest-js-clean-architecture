454597bea7f92391c9f730cedc99e2014b3ddb4c
Abhishek <abhishekkanichery@gmail.com>
2026-06-20 10:49:47 +0530

    chore: update folder stucture

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from '@nestjs/config';
-import { ShutdownService } from './lifecycle/shutdown.service';
+import { UsersModule } from './modules/users/users.module';
+import { ShutdownService } from './config/lifecycle/shutdown.service';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ShutdownService } from './lifecycle/shutdown.service';
       isGlobal: true,
     }),
     HealthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, ShutdownService],
