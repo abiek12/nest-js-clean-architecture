608c68e76bf738def10dd4722ab1a3ade0c5b23b
Abhishek <abhishekkanichery@gmail.com>
2026-06-26 16:12:59 +0530

    fix: test case

--- src/modules/users/presentation/http/controllers/users.controller.spec.ts
+++ src/modules/users/presentation/http/controllers/users.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserUseCase } from 'src/modules/users/application/use-cases/create-user.use-case';
+import { GetAllUsersUseCase } from 'src/modules/users/application/use-cases/get-all-users.use-case';
+import { GetUserByIdUseCase } from 'src/modules/users/application/use-cases/get-user-by-id.use-case';
+import { UpdateUserUseCase } from 'src/modules/users/application/use-cases/update-user.use-case';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
@@ -7,6 +11,12 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        { provide: CreateUserUseCase, useValue: { execute: jest.fn() } },
+        { provide: GetUserByIdUseCase, useValue: { execute: jest.fn() } },
+        { provide: GetAllUsersUseCase, useValue: { execute: jest.fn() } },
+        { provide: UpdateUserUseCase, useValue: { execute: jest.fn() } },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
